b908aedd815b5c59d8bbc8a1a9d2a050d081df57
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-08 19:47:42 -0300

    Criando o documento onde ficará armazenado a tela de visualização do produto, aproveitei e importei na Home, registrei nos componentesd e ja passei o produto na tag dele la proximo da div home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,52 @@
 <template>
   <div class="home">
-  <div class="Products-cards-container">
-  <!--//Itens talvez esteja errado-->
-   <ProductsSummaryCard 
-   v-for="product in items" 
-   :key="product.id" 
-   :product="product"
-   v-on:view-product="viewProduct($event)"
-   />
-  </div>
+    <ProductDrawer :product="product" />
+    <!--Possivel erro -->
+    <div class="Products-cards-container">
+      <!--//Itens talvez esteja errado-->
+
+      <ProductsSummaryCard
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+        v-on:view-product="viewProduct($event)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 //Itens talvez esteja errado
 import items from "../data/items.js";
-import ProductsSummaryCard from '../components/products/ProductsSummaryCard.vue';
-
+import ProductsSummaryCard from "../components/products/ProductsSummaryCard.vue";
+import ProductDrawer from "../components/products/ProductDrawer.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    ProductsSummaryCard
-
-  },  
+    ProductsSummaryCard,
+    ProductDrawer,
+  },
   data() {
     return {
       //Itens talvez esteja errado
       items: items,
-      product: null
-    }
+      product: null,
+    };
   },
   methods: {
-    viewProduct(product){
+    viewProduct(product) {
       this.product = product;
-      console.log(this.product)
-
-    }
-  }
-}
+      console.log(this.product);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-.Products-cards-container{
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
-
-
+.Products-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
