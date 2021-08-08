dcfc336f01a585edc4f4243d4de7e38923a3e7e6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-08 02:22:01 -0300

    Aqui eu criei o local onde ficará armazenado o array dos itens que eu irei retornar que serão importados no cart

---
+++ src/data/itens.js
@@ -0,0 +1,44 @@
+const item = [
+    {
+    id: 1,
+    name: "Spider Man 3",
+    Price: 10.00,
+    Genre: "Action",
+    Rating: "PG-13"
+
+},
+{
+    id: 2,
+    name: "El Camino",
+    Price: 20.00,
+    Genre: "Action, Drama",
+    Rating: "PG-18"
+
+},
+{
+    id: 3,
+    name: "Sonic The Movie",
+    Price: 20.00,
+    Genre: "Adventure",
+    Rating: "PG-10"
+
+},
+{
+    id: 4,
+    name: "Zootopia",
+    Price: 30.00,
+    Genre: "Action",
+    Rating: "PG-10"
+
+},
+{
+    id: 5,
+    name: "Star Wars Episode 3: Revenge of the Sith",
+    Price: 10.00,
+    Genre: "Action",
+    Rating: "PG-13"
+
+}
+]
+
+export default item;
