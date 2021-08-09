50158d81f2fe89400c9fe45916a655f867bf2f5e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 18:10:35 -0300

    Criando a página do carrinho da loja, ja criei um export que vai pegar os itens guardados dentro do getter na store/indes.js, lembrando que esses itens estão guardados dentro de um objeto no data/itens.js

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    Carrinho
-    
+    <h1>Carrinho</h1>
+
+    <div v-for="product in items" :key="product.id">
+      {{ product.name }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      return this.$store.getters.cartItems;
+    },
+  },
+};
+</script>
