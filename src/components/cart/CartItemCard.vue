c36d0d542588b74642c59418b63c282620e3dd88
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 19:01:45 -0300

    Criando o CartItemCard denro dos componentes, dentro desse cartItem ele vai mostara a quantiade de igens no carrinho, o nome dos produtos e o valor total. La embaixo eu criei um computed onde eu estou inserindo os Item_cost que vai retornar o valor total dos produtos com base na multiplicação do preço e da quantidade

---
+++ src/components/cart/CartItemCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cart-item-card">
+    <div class="header">
+    <h3> {{product.name}}</h3>
+    <h4>No carrinho: {{product.quantity}}</h4>
+    <h4>Custo Total: {{item_cost.toFixed(2)}}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['products'],
+    computed: {
+        item_cost(){
+            return this.product.price * this.product.quantity
+        }
+    }
+}
+</script>
