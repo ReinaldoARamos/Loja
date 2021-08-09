6d47cb491423467b4d44dc588b490170bcd1993b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 19:11:08 -0300

    Alinhando textos

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+<div>
+  <div class="Textos">
     <h1>Carrinho</h1>
+    
 <CartItemsCard
 v-for="product in items" 
 :key="product.id"
 :product="product"
 />
+
   </div>
+</div>
 </template>
 
 <script>
@@ -16,9 +20,17 @@ export default {
     CartItemsCard
   },
   computed: {
-    products() {
+    product() {
       return this.$store.getters.cartItems;
     },
   },
 };
+
 </script>
+
+<style land="scss">
+.Textos{
+  text-align: center;
+}
+
+</style>
