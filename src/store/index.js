04010f894853d4eb8240f4688de48fd0799293c0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 15:10:55 -0300

    Documentando o getter no index.js da store

--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,10 @@ export default createStore({
 
   getters: {
     ProductQuantity: (state) => (product) => {
+      //basicamente criei  o getter que tem a quantidade de produto que vai pegar de dentro do cart
+      //criei a const item que procura dentro do state.cart o index dos itens, pega o id e iguala ao produto
+      //se for encontrado um item ele retorna a quantidade, caso não, retorna nulo
+      
       const item = state.cart.find((i) => i.id === product.id);
 
       if (item) return item.quantity;
