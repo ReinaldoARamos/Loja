5ee3fdc8af6877d4990392fa1c8a0959dbd8ad8d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-08 23:10:33 -0300

    Era apenas um problema simples de fechamento de tags

--- src/components/products/ProductDrawer.vue
+++ src/components/products/ProductDrawer.vue
@@ -1,30 +1,25 @@
 <template>
-  <div
-    class="drawer-background"
-    :class="{ show: active }"
-    @click="$emit('close-product-drawer')"
-  />
+  <div class="drawer-background" :class="{ show: active }" @click="$emit('close-product-drawer')"/>
 
-  <div class="drawer" :class="{ show: active }">
-    <div class="drawer-close" @click="$emit('close-product-drawer')" />
-    X
-  </div>
+  <div class="drawer" :class="{ show: active }"> 
+    <div class="drawer-close" @click="$emit('close-product-drawer')"> X </div>
 
   <div v-if="product" class="product-details">
     <h3 class="text-center">{{ product.name }}</h3>
-    <h3 class="text-center">{{ product.Price.toFixed(2) }}</h3>
+    <h4 class="text-center">{{ product.Price.toFixed(2) }}</h4>
     <p class="Genre">{{ product.Genre }}</p>
     <p class="Rating">{{ product.Rating }}</p>
 
-    <div class="cart-total" v-if="product-total">
+    <div class="cart-total" v-if="product_total">
       <h3>No carrinho</h3>
-      <h4>{{ product-total }}</h4>
+      <h4>{{ product_total }}</h4>
     </div>
 
     <div class="button-container">
       <button class="remove">Remover</button>
       <button class="add">Adicionar</button>
     </div>
+  </div>
   </div>
 </template>
 
