ba929b972d075600f4c1fe6d58d7274139cbb740
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 15:07:27 -0300

    Criando o getter que retonar a quantidade de itens

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
-function updateLocalStorage(cart){
-  localStorage.setItem('cart', JSON.stringify(cart))
-
+function updateLocalStorage(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 export default createStore({
   state: {
-    
+    cart: [],
   },
-  mutations: {
+
+  getters: {
+    ProductQuantity: (state) => (product) => {
+      const item = state.cart.find((i) => i.id === product.id);
+
+      if (item) return item.quantity;
+      else return null;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
