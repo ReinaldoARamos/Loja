0b16c7cf3eae901136f4f8a9a11024416d4cd297
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 15:33:49 -0300

    Função de adicionar o carrinho sendo criada, basciametne ele recebe o state e o product, procura dentro do cart(que esta dentro do state) por um index e pega essa id e compara os valores com a do product, e o if serve para caso haja um item ele adicione a quantidade e caso não haja ele adicione 1

--- src/store/index.js
+++ src/store/index.js
@@ -14,14 +14,25 @@ export default createStore({
       //basicamente criei  o getter que tem a quantidade de produto que vai pegar de dentro do cart
       //criei a const item que procura dentro do state.cart o index dos itens, pega o id e iguala ao produto
       //se for encontrado um item ele retorna a quantidade, caso não, retorna nulo
-      
+
       const item = state.cart.find((i) => i.id === product.id);
 
       if (item) return item.quantity;
       else return null;
     },
   },
-  mutations: {},
+  mutations: {
+    addToCart(state, product){
+      let item  = state.cart.find(i => i.id === product.id)
+
+      if (item){
+        item.quantity++;
+      }
+      else{
+        state.cart.push({...product, quantity: 1})
+      }
+    }
+  },
   actions: {},
   modules: {},
 });
