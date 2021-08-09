7db7bd2ee0ed3f9ac752a7f8cef14eb4ea71046d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 14:47:27 -0300

    Criando uma função para adicionar itens ao carrinho de compras

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { createStore } from 'vuex'
 
+function updateLocalStorage(cart){
+  localStorage.setItem('cart', JSON.stringify(cart))
+
+}
+
 export default createStore({
   state: {
+    
   },
   mutations: {
   },
