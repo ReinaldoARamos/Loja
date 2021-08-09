b5c932567a1364545173dbefa6bd5c9c746cdd45
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-08 21:02:38 -0300

    Criando o vue da página da visualização dos produtos. Aqui eu coloquei as informações que irão aparecer, uma função para fechar a aba e também um campo que irá mostrar o total das compras

--- src/components/products/ProductDrawer.vue
+++ src/components/products/ProductDrawer.vue
@@ -1,3 +1,35 @@
 <template>
-    <div></div>
+  <div
+    class="drawer-background"
+    :class="{ show: active }"
+    @click="$emit('close-product-drawer')"
+  />
+
+  <div class="drawer" :show="{ show: active }">
+    <div class="drawer-close" @click="$emit('close-product-drawer')">
+      X
+    </div>
+
+    <div v-if="product" class="product-details">
+      <h3 class="text-center">{{ product.name }}</h3>
+      <h3 class="text-center">{{ product.price.toFixed(2) }}</h3>
+      <p class="Genre">{{ product.Genre }}</p>
+      <p class="Rating">{{ product.Rating }}</p>
+     
+     <div class="cart-total" v-if="product-total">
+     <h3>No carrinho</h3>
+     <h4>{{product_total}}</h4>
+     </div>
+
+     <div class="button-container">
+     <button class="remove">Remover</button>
+      <button class="add">Adicionar</button>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['product', 'active'],
+};
+</script>
