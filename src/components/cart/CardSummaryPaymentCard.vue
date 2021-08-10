35c0be4772486ecd43be9281f4184229b15889a7
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-10 00:03:36 -0300

    Estilizações finais. Projeto concluido

--- src/components/cart/CardSummaryPaymentCard.vue
+++ src/components/cart/CardSummaryPaymentCard.vue
@@ -7,12 +7,27 @@
 </template>
 <script>
 export default {
-    
-    computed:{
-       
-        cart_total(){
-            return this.$store.getters.cartTotal  
-        }
-    }
-}
+  computed: {
+    cart_total() {
+      return this.$store.getters.cartTotal;
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+button.view-product-button {
+  padding: 10px;
+  background-color: rgb(79, 169, 187);
+  border: none;
+  color: white;
+  font-weight: bold;
+  font-size: 1.15rem;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: red;
+  }
+}
+</style>
