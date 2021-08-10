eaf9b1b261bb083a935909422aef9fbbd1c25ab6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-09 23:22:57 -0300

    Criando a view das opções de pagamento e o total

--- src/components/cart/CardSummaryPaymentCard.vue
+++ src/components/cart/CardSummaryPaymentCard.vue
@@ -1,4 +1,16 @@
 <template>
-    <div>
-    </div>
+  <div class="cart-item-total">
+    <h3>Cart Total: ${{ cart_total.toFixed(2) }}</h3>
+
+    <button class="view-product-button">Pagar com cartão</button>
+  </div>
 </template>
+<script>
+export default {
+    computed:{
+        cart_total(){
+            return
+        }
+    }
+}
+</script>
